2ae988f328450bb93ac84c0058e76c234ff037e8
haeju0503 <163137843+haeju0503@users.noreply.github.com>
2024-03-12 21:51:34 +0900

    Update volume_calc.py
    print() 명령어 변경

--- volume_calc.py
+++ volume_calc.py
@@ -1,5 +1,5 @@
 width = float(input("가로:"))
 length = float(input("세로:"))
-height = float(input("높이:"))
+heigth = float(input("높이:"))
 
-print("박스의 부피는 %d 입니다.", width * length * height)  
+print("박스의 부피는", width * length * heigth, "입니다.")
